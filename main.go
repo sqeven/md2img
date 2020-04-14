135d9b27cbfb5c3a6e189480dab4a20f05db2f09
Sqeven <sqeven@qq.com>
2020-04-14 12:42:22 +0800

    fix: cmd

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 
 	md := NewMarkdown(text)
 
-	opts := []string{}
+	opts := []string{"--wideth 600g"}
 	if *css != "" {
 		var cssUrl string
 		u, _ := url.Parse(*css)
@@ -64,7 +64,7 @@ func main() {
 		} else {
 			cssUrl = u.String()
 		}
-		opts = append(opts, filepath.ToSlash(cssUrl))
+		opts = append(opts, "-q 66", filepath.ToSlash(cssUrl))
 	}
 
 	if *html {
